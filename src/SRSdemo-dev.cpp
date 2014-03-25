8d713bc3901fbc762e76ff3a184ea5120d2be641
sunghee <hellosunghee@gmail.com>
2014-03-25 19:02:37 +0900

    Shading 3D Mesh Model

--- src/SRSdemo-dev.cpp
+++ src/SRSdemo-dev.cpp
@@ -29,12 +29,14 @@ using namespace rapidxml;
 #define LIVER		3
 #define INSTRUMENT1 4
 #define INSTRUMENT2 5
+#define STOMACH_MESH	6
+
 
 
 #define BUFSIZE 512
 
 char *objectNames[] =
-{"Nothing", "Vessel", "Stomach", "Liver", "Instrument1", "Instrument2"};
+{"Nothing", "Vessel", "Stomach", "Liver", "Instrument1", "Instrument2","stomach-mesh"};
 
 static GLfloat theta[] = {0.0, 0.0, 0.0}; // Rotation (X,Y,Z)
 static GLint axis = 2; // Changed by the Mouse Callback Routine
@@ -65,6 +67,102 @@ string str_elements2;
 
 //int window_width = 640;
 //int window_height = 480;
+
+#define X .525731112119133606
+#define Z .850650808352039932
+
+static GLfloat vdata[12][3] = {
+   {-X, 0.0, Z}, {X, 0.0, Z}, {-X, 0.0, -Z}, {X, 0.0, -Z},
+   {0.0, Z, X}, {0.0, Z, -X}, {0.0, -Z, X}, {0.0, -Z, -X},
+   {Z, X, 0.0}, {-Z, X, 0.0}, {Z, -X, 0.0}, {-Z, -X, 0.0}
+};
+static GLuint tindices[20][3] = {
+   {0,4,1}, {0,9,4}, {9,5,4}, {4,5,8}, {4,8,1},
+   {8,10,1}, {8,3,10}, {5,3,8}, {5,2,3}, {2,7,3},
+   {7,10,3}, {7,6,10}, {7,11,6}, {11,0,6}, {0,1,6},
+   {6,1,10}, {9,0,11}, {9,11,2}, {9,2,5}, {7,2,11} };
+int i;
+
+
+
+void normalize(float v[3]) {
+   GLfloat d = sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
+   if (d == 0.0) {
+      cout << "zero length vector" << endl;
+      return;
+   }
+   v[0] /= d; v[1] /= d; v[2] /= d;
+}
+
+void normcrossprod(float v1[3], float v2[3], float out[3])
+{
+   GLint i, j;
+   GLfloat length;
+
+   out[0] = v1[1]*v2[2] - v1[2]*v2[1];
+   out[1] = v1[2]*v2[0] - v1[0]*v2[2];
+   out[2] = v1[0]*v2[1] - v1[1]*v2[0];
+   normalize(out);
+}
+
+void drawtriangle(float *v1, float *v2, float *v3)
+{
+   glBegin(GL_TRIANGLES);
+      glNormal3fv(v1); glVertex3fv(v1);
+      glNormal3fv(v2); glVertex3fv(v2);
+      glNormal3fv(v3); glVertex3fv(v3);
+   glEnd();
+}
+
+void subdivide(float *v1, float *v2, float *v3, long depth)
+{
+   GLfloat v12[3], v23[3], v31[3];
+   GLint i;
+
+   if (depth == 0) {
+      drawtriangle(v1, v2, v3);
+      return;
+   }
+   for (i = 0; i < 3; i++) {
+      v12[i] = v1[i]+v2[i];
+      v23[i] = v2[i]+v3[i];
+      v31[i] = v3[i]+v1[i];
+   }
+   normalize(v12);
+   normalize(v23);
+   normalize(v31);
+   subdivide(v1, v12, v31, depth-1);
+   subdivide(v2, v23, v12, depth-1);
+   subdivide(v3, v31, v23, depth-1);
+   subdivide(v12, v23, v31, depth-1);
+}
+void drawIcosahedron()
+{
+	  glBegin(GL_TRIANGLES);
+//	  for (i = 0; i < 20; i++) {
+//	        glNormal3fv(&vdata[tindices[i][0]][0]);
+//	        glVertex3fv(&vdata[tindices[i][0]][0]);
+//	        glNormal3fv(&vdata[tindices[i][1]][0]);
+//	        glVertex3fv(&vdata[tindices[i][1]][0]);
+//	        glNormal3fv(&vdata[tindices[i][2]][0]);
+//	        glVertex3fv(&vdata[tindices[i][2]][0]);
+//	  }
+	    GLfloat d1[3], d2[3], norm[3];
+	    for (int j = 0; j < 3; j++) {
+	       d1[j] = vdata[tindices[i][0]][j] - vdata[tindices[i][1]][j];
+	       d2[j] = vdata[tindices[i][1]][j] - vdata[tindices[i][2]][j];
+	    }
+	    normcrossprod(d1, d2, norm);
+	    glNormal3fv(norm);
+	    for (i = 0; i < 20; i++) {
+	       subdivide(&vdata[tindices[i][0]][0],
+	                 &vdata[tindices[i][1]][0],
+	                 &vdata[tindices[i][2]][0], 3);
+	    }
+	  glEnd();
+
+}
+
 
 void drawPolygon(int n =20 , int arg = 0, float mult = 1, float v = 1.0)
 {
@@ -200,6 +298,13 @@ void myInit(void)
   //glutSolidIcosahedron();
   glutSolidCube(2.0);
   glEndList();
+
+
+  glNewList(STOMACH_MESH, GL_COMPILE);
+  //glutSolidIcosahedron();
+  drawIcosahedron();
+  glEndList();
+
 }
 void init()
 {
@@ -492,14 +597,14 @@ void draw_front(void)
 	//////////////
 	// Stomach
 	/////////////
-	glLoadName(2);
+	glLoadName(STOMACH_MESH);
 	glPushMatrix();
-	if (theObject == 2)
+	if (theObject == 6)
 		highlightBegin();
 	glTranslatef(5, -7, 15);
 	//  if (!menu_inuse) {
 //		if (mouse_state == GLUT_LEFT) {
-		  if (theObject == 2) {
+		  if (theObject == 6) {
 //			theObject = 2;
 //			glRotatef(90, 0, 1, 0);
 			glRotatef((pow(rotate_speed,2)+pow(scaling,2)), rotate_speed, scaling, 0);
@@ -508,7 +613,7 @@ void draw_front(void)
 //		}
 	//  }
 	glRotatef(45, 0, 0, 1.0);
-	glScalef(2.0, 1.0, 1.0);
+	glScalef(6.0, 3.0, 3.0);
 	/* stomach label */
 	char text4[] = "stomach: ellipsoid";
 	x = 5, y = -5, z = 0;
@@ -519,8 +624,8 @@ void draw_front(void)
 	for(int i = 0; text4[i] != '\0'; i++)
 		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text4[i]);
 
-	glCallList(STOMACH);
-	if (theObject == 2)
+	glCallList(STOMACH_MESH);
+	if (theObject == 6)
 		highlightEnd();
 	glPopMatrix();
 
